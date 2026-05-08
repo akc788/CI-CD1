6140f6d3876686f3e8efc1e48cbd8ebd148cf64a
akc788 <akc03437@gmail.com>
2026-05-08 23:24:40 +0300

    Add health check endpoint

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+// Health check endpoint
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+// Optional version endpoint
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 const start = async () => {
   await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
